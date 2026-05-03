10758350ce133a8df07420f8a6aa86257f1b30ec
cdbrws <cdbrws@gmail.com>
2026-05-03 00:26:10 -0300

    Update index.js

--- src/data/knowledge/index.js
+++ src/data/knowledge/index.js
@@ -1,1 +1,13 @@
+import { FOOD_KNOWLEDGE } from '../foodKnowledge';
+import { GAMES_KNOWLEDGE } from '../gamesKnowledge';
+import { HISTORY_KNOWLEDGE } from '../historyKnowledge';
+import { MUSIC_KNOWLEDGE } from '../musicKnowledge';
+import { SAN_LUIS_KNOWLEDGE } from '../sanLuisKnowledge';
 
+export const CROCANTE_KNOWLEDGE = [
+  ...FOOD_KNOWLEDGE,
+  ...GAMES_KNOWLEDGE,
+  ...HISTORY_KNOWLEDGE,
+  ...MUSIC_KNOWLEDGE,
+  ...SAN_LUIS_KNOWLEDGE,
+];
