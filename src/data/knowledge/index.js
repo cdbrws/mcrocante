df7177eafd10109f5cff59debf557d6df0ffa6a2
cdbrws <cdbrws@gmail.com>
2026-05-03 01:08:03 -0300

    Refactor imports and add FAMILY_KIDS_KNOWLEDGE

--- src/data/knowledge/index.js
+++ src/data/knowledge/index.js
@@ -1,11 +1,14 @@
-import { FOOD_KNOWLEDGE } from '../foodKnowledge';
-import { GAMES_KNOWLEDGE } from '../gamesKnowledge';
-import { HISTORY_KNOWLEDGE } from '../historyKnowledge';
-import { MUSIC_KNOWLEDGE } from '../musicKnowledge';
-import { SAN_LUIS_KNOWLEDGE } from '../sanLuisKnowledge';
+import { FOOD_KNOWLEDGE } from './foodKnowledge';
+import { GAMES_KNOWLEDGE } from './gamesKnowledge';
+import { SAN_LUIS_KNOWLEDGE } from './sanLuisKnowledge';
+import { MUSIC_KNOWLEDGE } from './musicKnowledge';
+import { HISTORY_KNOWLEDGE } from './historyKnowledge';
+import { BOOKS_KNOWLEDGE } from './booksKnowledge';
 import { CHALLENGES_KNOWLEDGE } from './challengesKnowledge';
 import { UTILITY_KNOWLEDGE } from './utilityKnowledge';
+import { FAMILY_KIDS_KNOWLEDGE } from './familyKidsKnowledge';
 
+// BASE TOTAL DE CONOCIMIENTO CRO CANTE
 export const CROCANTE_KNOWLEDGE = [
   ...FOOD_KNOWLEDGE,
   ...GAMES_KNOWLEDGE,
@@ -14,5 +17,6 @@ export const CROCANTE_KNOWLEDGE = [
   ...HISTORY_KNOWLEDGE,
   ...BOOKS_KNOWLEDGE,
   ...CHALLENGES_KNOWLEDGE,
-  ...UTILITY_KNOWLEDGE
+  ...UTILITY_KNOWLEDGE,
+  ...FAMILY_KIDS_KNOWLEDGE
 ];
